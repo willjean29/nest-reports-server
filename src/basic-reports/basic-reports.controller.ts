2b12a342a1cab57bb932f397a8feded15090c064
Jean Osco <willjean29@gmail.com>
2024-09-30 13:18:13 -0500

    feat: create first pdf

--- src/basic-reports/basic-reports.controller.ts
+++ src/basic-reports/basic-reports.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { BasicReportsService } from './basic-reports.service';
+import { Response } from 'express';
 
 @Controller('basic-reports')
 export class BasicReportsController {
   constructor(private readonly basicReportsService: BasicReportsService) {}
   @Get()
-  async getBasicReports() {
-    return this.basicReportsService.getBasicReports();
+  async getBasicReports(@Res() res: Response) {
+    const pdfDoc = await this.basicReportsService.getBasicReports();
+    res.setHeader('Content-Type', 'application/pdf');
+    pdfDoc.pipe(res);
+    pdfDoc.end();
   }
 }
--- src/basic-reports/basic-reports.service.ts
+++ src/basic-reports/basic-reports.service.ts
@@ -1,12 +1,26 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-
+import PdfPrinter from 'pdfmake';
+import { TDocumentDefinitions } from 'pdfmake/interfaces';
+const fontDescriptors = {
+  Roboto: {
+    normal: 'fonts/Roboto/Roboto-Regular.ttf',
+    bold: 'fonts/Roboto/Roboto-Medium.ttf',
+    italics: 'fonts/Roboto/Roboto-Italic.ttf',
+    bolditalics: 'fonts/Roboto/Roboto-MediumItalic.ttf',
+  },
+};
 @Injectable()
 export class BasicReportsService extends PrismaClient implements OnModuleInit {
   onModuleInit() {
     this.$connect();
   }
   async getBasicReports() {
-    return { message: 'Hello from BasicReportsService!' };
+    const printer = new PdfPrinter(fontDescriptors);
+    const docDefinition: TDocumentDefinitions = {
+      content: ['Hello world!'],
+    };
+    const doc = printer.createPdfKitDocument(docDefinition);
+    return doc;
   }
 }
