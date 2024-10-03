b47d823ab4d9ae151b835e733a5e073575aed153
Jean Osco <willjean29@gmail.com>
2024-10-02 23:47:44 -0500

    refactor: header section

--- src/reports/sections/header.section.ts
+++ src/reports/sections/header.section.ts
@@ -8,25 +8,52 @@ interface HeaderSectionOptions {
   showDate?: boolean;
 }
 
+const logoContent: Content = {
+  image: 'assets/tucan-code-logo.png',
+  width: 60,
+  height: 60,
+  alignment: 'left',
+  margin: [20, 10],
+  background: 'red',
+};
+
+const dateContent: Content = {
+  text: DateFormatter.getFullDate(new Date()),
+  alignment: 'right',
+  margin: [20, 20],
+  width: 180,
+};
+
+const subTitleContent = (subTitle: string): Content => ({
+  text: subTitle,
+  style: {
+    bold: true,
+    fontSize: 16,
+    marginTop: 5,
+  },
+});
+
+const titleContent = (title: string): Content => ({
+  text: title,
+  style: {
+    bold: true,
+    fontSize: 22,
+  },
+});
+
 export const headerSection = (options: HeaderSectionOptions): Content => {
-  const { showLogo = true, showDate = true } = options;
-  const logo: Content = {
-    image: 'assets/tucan-code-logo.png',
-    width: 60,
-    height: 60,
-    alignment: 'left',
-    margin: [30, 10],
-    background: 'red',
-  };
-  const headerLogo: Content = showLogo ? logo : null;
-  const headerDate: Content = showDate
+  const { title, subTitle, showLogo = true, showDate = true } = options;
+  const headerLogo: Content = showLogo ? logoContent : null;
+  const headerDate: Content = showDate ? dateContent : null;
+  const headerSubtTitle: Content = subTitle ? subTitleContent(subTitle) : null;
+  const headerTitle: Content = title
     ? {
-        text: DateFormatter.getFullDate(new Date()),
-        alignment: 'right',
-        margin: [30, 10],
+        stack: [titleContent(title), headerSubtTitle],
+        margin: [80, 10, 0, 0],
+        alignment: 'center',
       }
     : null;
   return {
-    columns: [headerLogo, headerDate],
+    columns: [headerLogo, headerTitle, headerDate],
   };
 };
