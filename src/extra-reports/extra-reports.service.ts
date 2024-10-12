aabd1ec1659a13db2f5ce933c4bac1ad3b3bcc7f
Jean Osco <willjean29@gmail.com>
2024-10-11 23:03:04 -0500

    feat: add render complex html

--- src/extra-reports/extra-reports.service.ts
+++ src/extra-reports/extra-reports.service.ts
@@ -4,16 +4,27 @@ import { TDocumentDefinitions } from 'pdfmake/interfaces';
 import { getHtmlContent } from 'src/helpers/html-to-pdfmake';
 import { PrinterService } from 'src/printer/printer.service';
 import { headerSection } from 'src/reports/sections';
+import { footerSection } from '../reports/sections/footer.section';
 
 @Injectable()
 export class ExtraReportsService {
   constructor(private readonly printerService: PrinterService) {}
   async getBasicReports() {
-    const html = fs.readFileSync('src/reports/html/basic-report.html', 'utf8');
-    const content = getHtmlContent(html);
+    const html = fs.readFileSync(
+      'src/reports/html/basic-report-03.html',
+      'utf8',
+    );
+    const content = getHtmlContent(html, {
+      client: 'Jean Osco',
+    });
     const docDefinition: TDocumentDefinitions = {
-      header: headerSection({}),
+      header: headerSection({
+        title: 'Html to pdfmake',
+        subTitle: 'Conversión de html a pdfmake',
+      }),
+      pageMargins: [40, 80],
       content: content,
+      footer: footerSection,
     };
     const doc = this.printerService.createPdf(docDefinition);
     return doc;
--- src/helpers/html-to-pdfmake.ts
+++ src/helpers/html-to-pdfmake.ts
@@ -1,6 +1,13 @@
 import htmlToPdfmake from 'html-to-pdfmake';
 import { JSDOM } from 'jsdom';
-export const getHtmlContent = (html: string) => {
+
+interface ContentReplacer {
+  [key: string]: string;
+}
+export const getHtmlContent = (html: string, replacers: ContentReplacer) => {
   const { window } = new JSDOM();
+  Object.entries(replacers).forEach(([key, value]) => {
+    html = html.replaceAll(`{{ ${key} }}`, value);
+  });
   return htmlToPdfmake(html, { window });
 };
